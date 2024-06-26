1ec3498ff287658f0b35de6eecbbc0b368af1c71
Josh <111873936+JimbyJuice@users.noreply.github.com>
2024-06-26 18:40:31 +1000

    Update index.php

--- PHPDEMO/api/index.php
+++ PHPDEMO/api/index.php
@@ -1,12 +1,3 @@
 <?php
-header('Content-Type: application/json');
-
-if ($_SERVER['REQUEST_METHOD'] === 'GET'){
-    $response = array(
-        'message' => 'Hello World!'
-    );
-} else{
-    http_response_code(405);
-    echo json_encode(array('error' => 'Method Not Allowed'));
-}
-?>
+// api/index.php
+phpinfo();
