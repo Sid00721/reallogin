dbd13a191060a9e9a30fc69016cf39a54db5543f
Josh <111873936+JimbyJuice@users.noreply.github.com>
2024-06-27 11:38:27 +1000

    Update index.php

--- api/index.php
+++ api/index.php
@@ -22,7 +22,7 @@
  ini_set('session.gc_maxlifetime', 7200);
   // Your configuration block. Register your app on the Student Portal.
  // Don't leak your Client Secret. If you do, make sure you regenerate it through the Portal.
- $client->redirect_uri  = 'https://localhost/sbhslogin/sbhsdemo.php';
+$client->redirect_uri  = "https://phpdemo1.vercel.app/" ; //'https://localhost/sbhslogin/sbhsdemo.php'
  $client->client_id     = '01hy9zy7azf95bjyn0pn226sx5';
  $client->client_secret = 'jOxgsZ4UkCLe8b9-SKwSKftjNu3mQvR6-C2lx8GTqcApOx14jJNOD_RcNpDlbyHOQUMl4MTuNP50BK4O';
 
