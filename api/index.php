88cd2e6a0e929331901d99e20cdd1f35190da691
Josh <111873936+JimbyJuice@users.noreply.github.com>
2024-06-27 12:06:19 +1000

    Update index.php

--- api/index.php
+++ api/index.php
@@ -17,7 +17,7 @@ $client = new OAuth_Client_Class;
 
 // Your configuration block. Register your app on the Student Portal.
 // Don't leak your Client Secret. If you do, make sure you regenerate it through the Portal.
-$client->redirect_uri = "https://phpdemo1.vercel.app/api/index.php";
+$client->redirect_uri = "https://phpdemo1.vercel.app";//"https://phpdemo1.vercel.app/api/index.php";
 $client->client_id = '01hy9zy7azf95bjyn0pn226sx5';
 $client->client_secret = 'jOxgsZ4UkCLe8b9-SKwSKftjNu3mQvR6-C2lx8GTqcApOx14jJNOD_RcNpDlbyHOQUMl4MTuNP50BK4O';
 
